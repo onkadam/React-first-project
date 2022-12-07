70a4c4f17c4d60e8c76ba7e6ffa582565739e5bf
onkadam <onkark3737@gmail.com>
2022-12-07 11:30:45 +0530

    new

---
+++ src/ProfileCard.js
@@ -0,0 +1,15 @@
+function ProfileCard({title, handle}){
+
+//const {title, handle} = props;
+
+    return (
+        <div>
+            <div>Title is {title}</div>
+            <div>Title is {handle}</div>
+            <div>Title is {title}</div>
+            <div>Title is {handle}</div>
+        </div>
+    );
+}
+
+export default ProfileCard;
