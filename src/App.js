e1a2c53929e4339f8c87e67efd1af251eb42d9d3
onkadam <onkark3737@gmail.com>
2022-12-08 11:23:18 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import ProfileCard  from "./ProfileCard";
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./img/alexa.png";
+import CortanaImage from "./img/cortana.png";
+import SiriImage from "./img/siri.png";
 
-function App(){
-    return (
-       <div> 
-        <div>Personal Digital Assistants</div>
+function App() {
+  return (
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
 
-        <ProfileCard title = 'Alexa' handle = '@alexa99'/>
-        <ProfileCard title = 'Carton' handle = '@Cartom32'/>
-        <ProfileCard title = 'Siri' handle = '@siri01'/>
-         </div>   
-    );
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                discription="Alexa is created by Amazon and helps you by things"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Carton"
+                handle="@Cartom32"
+                image={CortanaImage}
+                discription="Cartona was made by Microsoft. Who knows what it is"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                discription="Siri was made by Apple.It is a virtual Assistant"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,15 +1,23 @@
-function ProfileCard({title, handle}){
+function ProfileCard({ title, handle, image, discription}) {
+  //const {title, handle} = props;
 
-//const {title, handle} = props;
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="img logo" />
+        </figure>
+      </div>
 
-    return (
-        <div>
-            <div>Title is {title}</div>
-            <div>Title is {handle}</div>
-            <div>Title is {title}</div>
-            <div>Title is {handle}</div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
         </div>
-    );
+        <div className="content">{discription}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileCard;
